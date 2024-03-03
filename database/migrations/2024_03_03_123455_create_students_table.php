f46ac554109e8fd71bc5555d99610cf5d1613d2a
hassan2112d <hassan2112d@aptechgdn.net>
2024-03-03 17:42:38 +0500

    update

--- database/migrations/2024_03_03_123455_create_students_table.php
+++ database/migrations/2024_03_03_123455_create_students_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table ->string('student_name');
+            $table ->string('student_class');
+            $table->float('student_percentage',3,1);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('students');
     }
 };
