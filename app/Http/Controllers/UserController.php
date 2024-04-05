161f599d534fd3a91ad8e95676ed629fe36d04ad
hassan2112d <hassan2112d@aptechgdn.net>
2024-04-06 02:39:43 +0500

    hello

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function showstudents(){
+
+        $users = DB::table('students')->get();
+        //return $users;
+        //dump($users);
+
+        return view("allusers",['data' => $users]);
+        
+    }
+
+    public function singlestudents($id){
+
+        $users = DB::table('students')->where('student_id',$id)->get();
+        return view('singleuser',['data' => $users]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fetchdata',[UserController::class,'showstudents']);
+
+Route::get('/student/{id}',[UserController::class,'singlestudents'])->name('view.student');
